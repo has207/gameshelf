7e902c5aa60a1923668402789c85bc51da0c0e32
Herman S. <429230+has207@users.noreply.github.com>
2025-04-24 15:49:20 +0900

    Clean up game items

--- controller.py
+++ controller.py
@@ -22,13 +22,9 @@ class SidebarRow(Gtk.Box):
     __gtype_name__ = "SidebarRow"
     label: Gtk.Label = Gtk.Template.Child()
     icon: Gtk.Image = Gtk.Template.Child()
-    selected_indicator: Gtk.Image = Gtk.Template.Child()
     
     def set_icon_name(self, icon_name):
         self.icon.set_from_icon_name(icon_name)
-    
-    def set_selected(self, selected):
-        self.selected_indicator.set_visible(selected)
 
 
 @Gtk.Template(filename=os.path.join(os.path.dirname(__file__), "layout", "details_panel.ui"))
@@ -58,31 +54,45 @@ class GameShelfWindow(Adw.ApplicationWindow):
     def __init__(self, app, controller):
         super().__init__(application=app)
         self.controller = controller
+    
+        # Debug to see if the UI template is loaded correctly
+        print("Sidebar ListView:", self.sidebar_listview)
+        print("Games Grid:", self.games_grid)
+        print("Details Panel:", self.details_panel)
+    
+        # Only continue if the template elements exist
+        if hasattr(self, 'sidebar_listview') and self.sidebar_listview is not None:
+            self.sidebar_store = Gio.ListStore(item_type=SidebarItem)
+            self.sidebar_store.append(SidebarItem("Games", "view-grid-symbolic"))
+        
+            # Add runners with appropriate icons
+            for runner in controller.get_runners():
+                icon_name = self._get_runner_icon(runner.id)
+                self.sidebar_store.append(SidebarItem(runner.id, icon_name))
 
-        self.sidebar_store = Gio.ListStore(item_type=SidebarItem)
-        self.sidebar_store.append(SidebarItem("Games", "view-grid-symbolic"))
+            factory = Gtk.SignalListItemFactory()
+            factory.connect("setup", self._setup_sidebar_item)
+            factory.connect("bind", self._bind_sidebar_item)
 
-        for runner in controller.get_runners():
-            icon_name = self._get_runner_icon(runner.id)
-            self.sidebar_store.append(SidebarItem(runner.id, icon_name))
+            selection = Gtk.SingleSelection(model=self.sidebar_store)
+            selection.connect("notify::selected", self._on_sidebar_selection)
 
-        factory = Gtk.SignalListItemFactory()
-        factory.connect("setup", self._setup_sidebar_item)
-        factory.connect("bind", self._bind_sidebar_item)
-
-        selection = Gtk.SingleSelection(model=self.sidebar_store)
-        selection.connect("notify::selected", self._on_sidebar_selection)
-        selection.set_selected(0)
-
-        self.sidebar_listview.set_model(selection)
-        self.sidebar_listview.set_factory(factory)
-
-        controller.bind_gridview(self.games_grid)
-        self.details_panel.set_reveal_flap(False)
-
-        selection_model = self.games_grid.get_model()
-        if isinstance(selection_model, Gtk.SingleSelection):
-            selection_model.connect("notify::selected-item", self._on_game_selected)
+            self.sidebar_listview.set_model(selection)
+            self.sidebar_listview.set_factory(factory)
+    
+        # Only bind grid if it exists
+        if hasattr(self, 'games_grid') and self.games_grid is not None:
+            controller.bind_gridview(self.games_grid)
+        
+        # Only setup details panel if it exists
+        if hasattr(self, 'details_panel') and self.details_panel is not None:
+            self.details_panel.set_reveal_flap(False)
+        
+            # Only setup selection model if games_grid exists
+            if hasattr(self, 'games_grid') and self.games_grid is not None:
+                selection_model = self.games_grid.get_model()
+                if isinstance(selection_model, Gtk.SingleSelection):
+                    selection_model.connect("notify::selected-item", self._on_game_selected)
 
     def _get_runner_icon(self, runner_id):
       icon_map = {
@@ -111,11 +121,6 @@ class GameShelfWindow(Adw.ApplicationWindow):
         item = list_item.get_item()
         row.label.set_label(item.name.capitalize())
         row.set_icon_name(item.icon_name)
-
-        # Handle selection state
-        selection_model = self.sidebar_listview.get_model()
-        selected = selection_model.get_selected() == list_item.get_position()
-        row.set_selected(selected)
 
     def _on_game_selected(self, selection, param):
         selected_item = selection.get_selected_item()
@@ -236,21 +241,29 @@ class GameShelfController:
         self.populate_games()
 
     def _on_factory_setup(self, factory, list_item):
+        # Create a simple container box to hold our game items
         box = Gtk.Box(orientation=Gtk.Orientation.VERTICAL)
         list_item.set_child(box)
 
     def _on_factory_bind(self, factory, list_item):
         box = list_item.get_child()
-        while box.get_first_child():
-            box.remove(box.get_first_child())
-        game_item = self.games_model.get_item(list_item.get_position())
-        box.append(game_item)
+        # Remove any existing children
+        child = box.get_first_child()
+        while child:
+            box.remove(child)
+            child = box.get_first_child()
+
+        # Add our game item
+        position = list_item.get_position()
+        if position < self.games_model.get_n_items():
+            game_item = self.games_model.get_item(position)
+            box.append(game_item)
 
     def populate_games(self, filter_runner: Optional[str] = None):
         self.games_model.remove_all()
         games = self.get_games()
         if filter_runner:
-            games = [g for g in games if g.runner.lower() == filter_runner.lower()]
+            games = [g for g in games if g.runner == filter_runner]
         for game in games:
             game_item = self.create_game_widget(game)
             self.games_model.append(game_item)
